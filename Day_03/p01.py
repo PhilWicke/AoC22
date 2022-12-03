37bb8c9bfd3f2860ea6983234c82bccea052e8e5
PhilWicke <philippwicke@web.de>
2022-12-03 14:47:56 +0100

    brushed D03p01

--- Day_03/p01.py
+++ Day_03/p01.py
@@ -5,8 +5,6 @@ count = 0
 for line in lines:
     midpoint = int(len(line)/2)
     x = str(set([i for i in line[:midpoint] if i in line[midpoint:]]))[2]
-    if x.isupper():
-        count += ord(x) - 38
-    if x.islower():
-        count += ord(x) - 96
+    if x.isupper(): count += ord(x) - 38
+    if x.islower(): count += ord(x) - 96
 print(count)
