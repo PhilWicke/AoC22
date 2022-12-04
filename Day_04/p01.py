3999c5d3dfb58ed8852e322f483959aedea2fe3b
PhilWicke <philippwicke@web.de>
2022-12-04 12:37:53 +0100

    Day04

---
+++ Day_04/p01.py
@@ -0,0 +1,14 @@
+with open("data.txt", "r") as f_in:
+    lines = f_in.readlines()
+    lines = [line.strip() for line in lines]
+
+count = 0
+for line in lines:
+    a, b = line.split(",")
+    a, b = a.split("-"), b.split("-")
+    if int(a[0]) >= int(b[0]) and int(a[1]) <= int(b[1]):
+        count+=1
+    elif int(b[0]) >= int(a[0]) and int(b[1]) <= int(a[1]):
+        count+=1
+
+print(count)
